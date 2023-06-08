393f792c19e278ecdb0a88cbb0d42b6d13feaf8d
Jessica Pinheiro <jessicasilva.js1314@gmail.com>
2023-06-08 12:06:46 -0300

    aprendizado

---
+++ teste1/app.js
@@ -0,0 +1,35 @@
+//defineos subtipos do objeto
+var pedido = [
+    {
+        "nome": "hamburguer",
+        "valor": 12
+    },
+    {
+        "nome": "batata frita",
+        "valor": 12
+    },
+    {
+        "nome": "cheesebacon",
+        "valor": 12
+    },
+    {
+        "nome": "xsalada",
+        "valor": 12
+    }
+];
+var bebida = [
+    {
+        "nome": "coca",
+        "valor": 12
+    },
+    {
+        "nome": "fanta",
+        "valor": 12
+    },
+    {
+        "nome": "tubaina",
+        "valor": 12
+    }
+];
+var soma = bebida[0].valor + bebida[2].valor;
+console.log(soma);
